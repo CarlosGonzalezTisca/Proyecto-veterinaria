9e357624f117c4b85dd6b96f256dc239a23398b7
ArsalexD <67302059+ArsalexD@users.noreply.github.com>
2020-07-17 23:20:01 -0500

    wasa

--- Veterinaria-mascotas-saludables-bk/routes/empleado.js
+++ Veterinaria-mascotas-saludables-bk/routes/empleado.js
@@ -5,6 +5,9 @@ const mongoose=require('mongoose');
 const ClienteSch= mongoose.model('empleado');
 const{check,ValidacionesResult, validationResult}=require('express-validator');
 const bcrypt = require ('bcrypt') ;  
+/*Hola que pedo*/
+
+
 
 /* GET users listing. 
 router.get('/', async(req, res, next)=> {
